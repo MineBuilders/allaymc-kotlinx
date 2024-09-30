7bb4d65507ae05997ce51aea98f3eff55601167c
Cdm2883 <Cdm2883@hotmail.com>
2024-09-30 18:29:55 +0800

    feat: control versions at the same place

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,22 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.gradleup.shadow) apply false
 }
+
+subprojects {
+    afterEvaluate {
+
+        if (projectDir.resolve("src/main/resources/plugin.json").exists()) {
+            val version = rootProject.libs.versions.allaymc.kotlinx.get()
+
+            tasks.named("processResources") {
+                doLast {
+                    val origin = file("src/main/resources/plugin.json")
+                    val processed = file("${layout.buildDirectory.get()}/resources/main/plugin.json")
+                    val content = origin.readText().replace("\${version}", version)
+                    processed.writeText(content)
+                }
+            }
+        }
+
+    }
+}
