75bbc2d4ca85ffc2d57f36090f78d468498dbfe8
Cdm2883 <Cdm2883@hotmail.com>
2024-09-30 15:28:15 +0800

    feat: empty plugin

--- plugin/src/main/kotlin/vip/cdms/allaymc/kotlinx/KotlinxPlugin.kt
+++ plugin/src/main/kotlin/vip/cdms/allaymc/kotlinx/KotlinxPlugin.kt
@@ -3,8 +3,4 @@ package vip.cdms.allaymc.kotlinx
 import org.allaymc.api.plugin.Plugin
 
 @Suppress("unused")
-class KotlinxPlugin : Plugin() {
-    override fun onLoad() = pluginLogger.info("KotlinPluginTemplate loaded!")
-    override fun onEnable() = pluginLogger.info("KotlinPluginTemplate enabled!")
-    override fun onDisable() = pluginLogger.info("KotlinPluginTemplate disabled!")
-}
+class KotlinxPlugin : Plugin()
