0715c276f73b5066044f48c25302747c5c93fe43
Cdm2883 <Cdm2883@hotmail.com>
2024-10-01 15:40:01 +0800

    build: some jitpack problems...

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
     repositories {
         mavenCentral()
         maven("https://jitpack.io/")
+        maven("https://www.jitpack.io/")
         maven("https://repo.opencollab.dev/maven-releases/")
         maven("https://repo.opencollab.dev/maven-snapshots/")
         maven("https://storehouse.okaeri.eu/repository/maven-public/")
