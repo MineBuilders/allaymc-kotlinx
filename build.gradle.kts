c9c3210274ef93390f8e94e36d15c922a14138be
Cdm2883 <Cdm2883@hotmail.com>
2024-10-03 20:54:30 +0800

    build: enable context receivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
-    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.gradleup.shadow) apply false
@@ -9,6 +9,10 @@ plugins {
 
 subprojects {
     afterEvaluate {
+
+        kotlin.compilerOptions.freeCompilerArgs.addAll(
+            "-Xcontext-receivers",  // https://kotlinlang.org/docs/whatsnew2020.html#phased-replacement-of-context-receivers-with-context-parameters
+        )
 
         if (projectDir.resolve("src/main/resources/plugin.json").exists()) {
             val version = rootProject.libs.versions.allaymc.kotlinx.get()
