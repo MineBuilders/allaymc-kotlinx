b3c6d821ffce6775178b9feb662b42faa9ee5793
Cdm2883 <Cdm2883@hotmail.com>
2025-02-09 23:26:33 +0800

    build: setup jitpack

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    id("maven-publish")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("core") {
+            from(components["java"])
+            groupId = "com.github.MineBuilders.allaymc-kotlinx"
+            artifactId = "core"
+            version = project.version.toString()
+        }
+    }
 }
